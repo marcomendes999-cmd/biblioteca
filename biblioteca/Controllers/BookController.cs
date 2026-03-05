13e0532bef6f9132ed171e7e82fd1f9b991139b7
marli29 <marco.mendes@marli.pt>
2026-03-05 18:44:58 +0000

    criação de categorias

--- biblioteca/Controllers/BookController.cs
+++ biblioteca/Controllers/BookController.cs
@@ -7,22 +7,36 @@ namespace biblioteca.Controllers
     public class BookController : Controller
     {
         private readonly ExcelBookService _excelService;
+        private readonly ExcelCategoriaService _categoriaService;
 
-        public BookController(ExcelBookService excelService)
+
+        public BookController(ExcelBookService excelService, ExcelCategoriaService excelServiceCat)
         {
             _excelService = excelService;
+            _categoriaService = excelServiceCat;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? categoria)
         {
             var books = _excelService.GetBooks();
+
+            var listacategoria = _categoriaService.GetCategorias();
+
+            ViewBag.Categorias = listacategoria;
+
+            if (categoria != null)
+            {
+                books = books.Where(h => h.CategoriaId == categoria).ToList();
+            }
             return View(books);
         }
 
 
         public IActionResult New()
         {
-   
+            var listacategoria = _categoriaService.GetCategorias();
+
+            ViewBag.Categorias = listacategoria;
             return View();
         }
 
@@ -31,6 +45,8 @@ namespace biblioteca.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Book book)
         {
+           
+
             if (ModelState.IsValid)
             {
                 // Define datas automáticas se não vierem preenchidas
@@ -41,12 +57,21 @@ namespace biblioteca.Controllers
 
                 return RedirectToAction("Index");
             }
+            else
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors);
+            }
 
+           
             return View(book);
         }
 
         public IActionResult Edit(int id)
         {
+            var listacategoria = _categoriaService.GetCategorias();
+
+            ViewBag.Categorias = listacategoria;
+
             var book = _excelService.GetBooks().FirstOrDefault(b => b.Id == id);
             if (book == null)
                 return NotFound();
